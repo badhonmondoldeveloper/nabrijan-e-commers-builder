1e7679faa368028812efe65e0af53553b18a9283
badhondev <badhondev@gmail.com>
2026-09-12 18:16:36 -0400

    Update Prisma singleton and next config

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
+if (!process.env.PRISMA_CLIENT_ENGINE_TYPE) {
+  process.env.PRISMA_CLIENT_ENGINE_TYPE = 'library';
+}
+if (!process.env.PRISMA_CLI_QUERY_ENGINE_TYPE) {
+  process.env.PRISMA_CLI_QUERY_ENGINE_TYPE = 'library';
+}
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
